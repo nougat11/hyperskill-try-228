9ac8b7bdef0b1786f86977e6141b6650dc4e35d8
nougat11 <30313702+nougat11@users.noreply.github.com>
2023-08-27 01:40:29 +0300

    Create stage2.py
    Theory
    Each new stage of a project is a step up in difficulty. The main idea, though, remains the same. You need to take your code from the first stage and add new features. Find the description of these features below.
    
    Description
    A month has passed since the opening of your shop. Let's calculate the total earnings for this period! You know the total earned amount for each item:
    
    Item name
    
    Earned amount
    
    Bubblegum
    
    $202
    
    Toffee
    
    $118
    
    Ice cream
    
    $2250
    
    Milk chocolate
    
    $1680
    
    Doughnut
    
    $1075
    
    Pancake
    
    $80
    
    Use it and find the total income in the first month.
    
    Objectives
    In this stage, your program should:
    
    Print the Earned amount: line.
    
    Print the item names and the earned amount for each of them;
    
    Print the total earnings as shown below. Replace 0.0 with the actual total sum:
    
    Income: $0.0
    Example
    Example 1: output at the end of this stage (figures may vary)
    
    Earned amount:
    Bubblegum: $202
    Toffee: $118
    Ice cream: $2250
    Milk chocolate: $1680
    Doughnut: $1075
    Pancake: $80
    
    Income: $7777.0

---
+++ python-oop/my-first-project/stage2.py
@@ -0,0 +1,17 @@
+# Write your code here
+bubblegum = 202
+toffee = 118
+ice_cream = 2250
+milk_chocolate = 1680
+doughnut = 1075
+pancake = 80
+print(f"""
+Earned amount:
+Bubblegum: ${bubblegum}
+Toffee: ${toffee}
+Ice cream: ${ice_cream}
+Milk chocolate: ${milk_chocolate}
+Doughnut: ${doughnut}
+Pancake: ${pancake}
+
+Income: ${bubblegum + toffee + ice_cream + milk_chocolate + doughnut + pancake}""")
