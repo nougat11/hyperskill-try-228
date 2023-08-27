76fad396fa385d59bdfd470fbeef97c1a188d454
nougat11 <30313702+nougat11@users.noreply.github.com>
2023-08-27 14:46:57 +0300

    Create stage2.py

---
+++ python-oop/last-pencil/stage2.py
@@ -0,0 +1,6 @@
+print("How many pencils would you like to use:")
+pencils = int(input())
+print('Who will be the first (John, Jack):')
+name = input()
+print('|' * pencils)
+print(f'{name} is going first!')
