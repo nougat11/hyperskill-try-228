fe306116571c37e64f9250432679f5c2a0b10720
nougat11 <30313702+nougat11@users.noreply.github.com>
2023-08-27 11:58:19 +0300

    Create stage1.py

---
+++ python-oop/loan-calculator/stage1.py
@@ -0,0 +1,8 @@
+loan_principal = 'Loan principal: 1000'
+final_output = 'The loan has been repaid!'
+first_month = 'Month 1: repaid 250'
+second_month = 'Month 2: repaid 250'
+third_month = 'Month 3: repaid 500'
+print(loan_principal, first_month, second_month, third_month, final_output, sep='\n')
+
+# write your code here
