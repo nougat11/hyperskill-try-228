c2374d83b3f5b635eadd37961f19f03b2c382f36
nougat11 <30313702+nougat11@users.noreply.github.com>
2023-08-27 17:50:19 +0300

    Create stage5.py

---
+++ python-oop/last-pencil/stage5.py
@@ -0,0 +1,52 @@
+from random import randint
+print("How many pencils would you like to use:")
+pencils = None
+while True:
+    pencils = input()
+    if not pencils.isnumeric():
+        print('The number of pencils should be numeric')
+    elif pencils == '0':
+        print('The number of pencils should be positive')
+    else:  
+        pencils = int(pencils)
+        break
+        
+players = ['John', 'Jack']
+print('Who will be the first (John, Jack):')
+name = None
+while True:
+    name = input()
+    if name not in players:
+        print(f'Choose between {" and ".join(players)}')
+    else:
+        break
+index = players.index(name)
+while pencils > 0:
+    print('|' * pencils)
+    print(f"{players[index]}'s turn")
+    if index == 1:
+        if pencils % 4 == 1:
+            count = randint(1, 3)
+        elif pencils == 1:
+            count = 1
+        else:
+            count = (pencils % 4 + 4 - 1) % 4
+        print(count)
+    else:        
+        while True:
+            count = input()
+            if count not in ('1', '2', '3'):
+                print("Possible values: '1', '2' or '3'")
+            else:
+                count = int(count)
+                if count > pencils:
+                    print('Too many pencils were taken')
+                else:
+                    
+                    break
+            
+    pencils -= count
+    index += 1
+    index %= len(players)
+print(f'{players[index]} won')
+    
